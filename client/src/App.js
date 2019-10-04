d3aec61d977a9ab18d1fe25cbbc039e150c928ce
Jackie Atwood <takalaj@verizon.net>
2019-10-04 13:14:16 -0400

    Rendered data, Darkmode(hooks), class comps, answers.md done, minor styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Data from "./components/Data";
+import NavBar from "./hooks/NavBar";
+import { Darker } from "./components/Styles";
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      things: ""
+  
+    };
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Darker className="toggleBox">
+        <p>Too Bright? Click me.</p>
+        <NavBar />
+      </Darker>
+
+      <div>
+        <h1>Here's Some Useless Information!</h1>
+        <Data 
+            things={this.state.things}
+            />
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
